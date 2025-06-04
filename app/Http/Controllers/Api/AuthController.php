8607aa0b9cf42d6193c88fba4ea37e9a243861f2
Asim Irfan <rajputasim721@gmail.com>
2025-06-04 22:02:28 +0500

    user model

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\User;  // user model
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
